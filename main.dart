1686037caec5426e3b51512e92dd28bf32c5257b
CadeMichael <cade.lueker@gmail.com>
2021-01-19 23:45:26 +0900

    1rm

--- main.dart
+++ main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'workoutMath.dart';
 
 void main() => runApp(
       MaterialApp(
@@ -14,15 +15,83 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
+  final repCon = new TextEditingController();
+  final weightCon = new TextEditingController();
+  var _oneRepMax = "";
+
+  void getMax() {
+    String repS = repCon.value.text;
+    int reps = int.parse(repS);
+    String weightS = weightCon.value.text;
+    double weight = (double.parse(weightS));
+
+    setState(() {
+      int rm = WorkoutMath.oneRepMax(reps, weight).floor();
+      this._oneRepMax = rm.toString();
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          title: Text("Weight Planner"),
+          title: Text(
+            "Weight Planner",
+            textAlign: TextAlign.center,
+          ),
+          centerTitle: true,
         ),
-        drawer: Drawer(),
-        body: Container(),
+        body: Column(
+          children: [
+            Container(
+              padding: EdgeInsets.fromLTRB(8, 32, 8, 64),
+              child: Text(
+                "One Rep Max Calculator",
+                textScaleFactor: 2,
+              ),
+            ),
+            Container(
+              width: 100,
+              padding: EdgeInsets.all(8),
+              child: TextField(
+                controller: repCon,
+                keyboardType: TextInputType.number,
+                decoration: InputDecoration(
+                  labelText: 'Reps',
+                  labelStyle: TextStyle(fontWeight: FontWeight.bold),
+                  border: OutlineInputBorder(
+                      borderRadius:
+                          const BorderRadius.all(Radius.circular(10))),
+                ),
+              ),
+            ),
+            Container(
+              width: 100,
+              padding: EdgeInsets.all(8),
+              child: TextField(
+                controller: weightCon,
+                keyboardType: TextInputType.number,
+                decoration: InputDecoration(
+                  labelText: 'Weight',
+                  labelStyle: TextStyle(fontWeight: FontWeight.bold),
+                  border: OutlineInputBorder(
+                      borderRadius:
+                          const BorderRadius.all(Radius.circular(10))),
+                ),
+              ),
+            ),
+            Container(
+              child: _oneRepMax == ""
+                  ? Text("your one rep Max")
+                  : Text(_oneRepMax),
+            ),
+            FloatingActionButton(
+                child: Icon(Icons.autorenew),
+                splashColor: Colors.red,
+                onPressed: getMax),
+          ],
+        ),
       ),
     );
   }
---
+++ workoutMath.dart
@@ -0,0 +1,22 @@
+// this class handles the simple equations by which one rep max and the
+// intensities are calculated
+
+class WorkoutMath {
+  static double oneRepMax(int reps, double weight) {
+    double rm = weight / (1.0278 - 0.0278 * reps);
+    return rm;
+  }
+
+  static List<double> relativeIntensity(reps, weight, rm) {
+    double maxWeight = rm * (1.0278 - 0.0278 * reps);
+    double ai = (weight) / rm;
+    double ri = ai / (maxWeight / rm);
+    return [ai, ri];
+  }
+
+  static double increasedAi(int reps, double desiredRi) {
+    double mi = (1.0278 - 0.0278 * reps);
+    double newAi = (mi * desiredRi) / 100;
+    return newAi;
+  }
+}
